222abc1f70c5dff54b2ac84ae3ed21a4249d9b7f
hftamayo <hftamayo@gmail.com>
2023-01-19 10:12:12 -0600

    adding group model import

--- lib/ui/add_group_screen.dart
+++ lib/ui/add_group_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterreminder/models/group.dart';
 
 class AddGroupScreen extends StatefulWidget {
   const AddGroupScreen({super.key});
@@ -12,7 +13,21 @@ class _AddGroupScreenState extends State<AddGroupScreen> {
   final textController = TextEditingController();
   String? errorMessage;
 
-  void _onSave() {}
+  void _onSave() {
+    final name = textController.text.trim();
+    if (name.isEmpty) {
+      setState(() {
+        errorMessage = 'Name is required';
+      });
+      return;
+    } else {
+      setState(() {
+        errorMessage = null;
+      });
+    }
+    final result = Group(name: name, color: selectedColor.value);
+    Navigator.of(context).pop(result);
+  }
 
   @override
   Widget build(BuildContext context) {
