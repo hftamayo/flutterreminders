aa07b14c17f282b852cb74db11cd63bbb4f253bd
hftamayo <hftamayo@gmail.com>
2023-01-21 11:18:27 -0600

    extending this class to UI

--- lib/ui/add_group_screen.dart
+++ lib/ui/add_group_screen.dart
@@ -38,9 +38,114 @@ class _AddGroupScreenState extends State<AddGroupScreen> {
         height: MediaQuery.of(context).size.height / 1.5,
         width: MediaQuery.of(context).size.width,
         child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: []),
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                color: selectedColor,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.group,
+                      size: 60,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: TextField(
+                        controller: textController,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                        decoration: const InputDecoration(
+                          hintText: 'Group name',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.white,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(
+                      height: 20,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          errorMessage ?? '',
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                      child: Text('SELECT COLOR'),
+                    ),
+                    Expanded(
+                      child: GridView.builder(
+                        scrollDirection: Axis.horizontal,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                        ),
+                        itemCount: Colors.primaries.length,
+                        itemBuilder: (context, index) {
+                          final color = Colors.primaries[index];
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15.0, vertical: 5),
+                            child: InkWell(
+                              onTap: () {
+                                setState(() {
+                                  selectedColor = color;
+                                });
+                              },
+                              child: CircleAvatar(
+                                backgroundColor: color,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                      child: MaterialButton(
+                        color: Colors.blue,
+                        child: const Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Text(
+                            'Create Group',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        onPressed: _onSave,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
