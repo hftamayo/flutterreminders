0ad1b763917e507703ab7867201d7a138a82658a
hftamayo <hftamayo@gmail.com>
2023-01-21 10:52:57 -0600

    name refactoring and class starting point

---
+++ lib/ui/tasks_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutterreminder/models/group.dart';
+import 'package:flutterreminder/models/task.dart';
+import 'package:flutterreminder/objectbox.g.dart';
+
+class TasksScreen extends StatefulWidget {
+  const TasksScreen({super.key});
+
+  @override
+  State<TasksScreen> createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
